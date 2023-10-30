a564e6b209b465a5727453065d5a2e00fa0ba128
Gérard Mansoif <gitlab.81tc5@aleeas.com>
2023-10-30 21:17:48 +0100

    Fonction + variable maxPosition

--- script.js
+++ script.js
@@ -1,132 +1,67 @@
+// Selecteur + variables
 const barreDroite = document.querySelector(".droite");
 const barreGauche = document.querySelector(".gauche");
 const image = document.querySelector(".image");
 const nombre = document.querySelector(".position");
+const maxPosition = 7;
 let position = 1;
 
-// function update(position, image, nombre) {
-//     image.src = `img/${position}.jpg`
-//     nombre.innerHTML = `Image : ${position} / 7`
-// }
+// La fonction pour changer d'image
+function update(position, image, nombre, maxPosition) {
+    image.classList.remove("carrouselAnim");
+    void image.offsetWidth; 
+    image.classList.add("carrouselAnim");
+    image.src = `img/${position}.jpg`;
+    nombre.innerHTML = `Image : ${position} / ${maxPosition}`;
+}
 
-// barreDroite.addEventListener("click", () => { 
-//     if (position < 7) {
-//         position++;
-//         update(position, image, nombre);
-//     }
-//     else {
-//         position = 1;
-//         update(position, image, nombre);
-// });
+// Les changement aves les boutons*/
+barreDroite.addEventListener("click", () => { 
+    if (position < maxPosition) {
+        position++;
+        update(position, image, nombre, maxPosition);
+    }
+    else {
+        position = 1;
+        update(position, image, nombre, maxPosition);
+    }
+});
 
+barreGauche.addEventListener("click", () => { 
+    if (position == 1) {
+        position = maxPosition
+        update(position, image, nombre, maxPosition);
+    }
+    else if (position > 1) {
+        position--
+        update(position, image, nombre, maxPosition);
+        }
+});
+
+
+// Les changement avec les flèches du clavier
 document.onkeydown = function(event) {
     switch (event.key) {
         case "ArrowRight":
-        if (position < 7) {
-            image.classList.remove("carrouselAnim");
-            void image.offsetWidth; 
-            image.classList.add("carrouselAnim");
+        if (position < maxPosition) {
             position++
-            image.src = `img/${position}.jpg`
-            nombre.innerHTML = `Image : ${position} / 7`
+            update(position, image, nombre, maxPosition);
         }
         else {
-            image.classList.remove("carrouselAnim");
-            void image.offsetWidth; 
-            image.classList.add("carrouselAnim");
             position = 1
-            image.src = `img/${position}.jpg`
-            nombre.innerHTML = `Image : ${position} / 7`
+            update(position, image, nombre, maxPosition);
             }
         break;
         case "ArrowLeft":
             if (position == 1) {
-                image.classList.remove("carrouselAnim");
-                void image.offsetWidth; 
-                image.classList.add("carrouselAnim");
-                position = 7
-                image.src = `img/${position}.jpg`
-                nombre.innerHTML = `Image : ${position} / 7`
+                position = maxPosition
+                update(position, image, nombre, maxPosition);
             }
             else if (position > 1) {
-                image.classList.remove("carrouselAnim");
-                void image.offsetWidth; 
-                image.classList.add("carrouselAnim");
                 position--
-                image.src = `img/${position}.jpg`
-                nombre.innerHTML = `Image : ${position} / 7`
+                update(position, image, nombre, maxPosition);
                 }
         break;
     }
 };
-
-// document.addEventListener("keydown", function(event) {
-//     if (event.key == "ArrowRight"){
-//         if (position < 7) {
-//             position++
-//             image.src = `img/${position}.jpg`
-//             nombre.innerHTML = `Image : ${position} / 7`
-//         }
-//         else {
-//             position = 1
-//             image.src = `img/${position}.jpg`
-//             nombre.innerHTML = `Image : ${position} / 7`
-//             }
-//     }
-//     });
-
-//     document.addEventListener("keydown", function(event) {
-//         if (event.key == "ArrowLeft"){
-//             if (position == 1) {
-//                 position = 7
-//                 image.src = `img/${position}.jpg`
-//                 nombre.innerHTML = `Image : ${position} / 7`
-//             }
-//             else if (position > 1) {
-//                 position--
-//                 image.src = `img/${position}.jpg`
-//                 nombre.innerHTML = `Image : ${position} / 7`
-//                 }
-//         }
-//         });
-
-
-barreDroite.addEventListener("click", () => { 
-    if (position < 7) {
-        image.classList.remove("carrouselAnim");
-        void image.offsetWidth; 
-        image.classList.add("carrouselAnim");
-        position++
-        image.src = `img/${position}.jpg`
-        nombre.innerHTML = `Image : ${position} / 7`
-    }
-    else {
-        image.classList.remove("carrouselAnim");
-        void image.offsetWidth; 
-        image.classList.add("carrouselAnim");
-        position = 1
-        image.src = `img/${position}.jpg`
-        nombre.innerHTML = `Image : ${position} / 7`
-        }
-});
-
-
-barreGauche.addEventListener("click", () => { 
-    if (position == 1) {
-        image.classList.remove("carrouselAnim");
-        void image.offsetWidth; 
-        image.classList.add("carrouselAnim");
-        position = 7
-        image.src = `img/${position}.jpg`
-        nombre.innerHTML = `Image : ${position} / 7`
-    }
-    else if (position > 1) {
-        image.classList.remove("carrouselAnim");
-        void image.offsetWidth; 
-        image.classList.add("carrouselAnim");
-        position--
-        image.src = `img/${position}.jpg`
-        nombre.innerHTML = `Image : ${position} / 7`
-        }
-});
 
